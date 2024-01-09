02e9f57d6fb69bc771724e20c74db090f07afdfb
Rodrigo Fassi <rodrifassi7@gmail.com>
2024-01-09 18:14:33 -0300

    desarrollo header, hero, contact y footer de eleven

---
+++ tailwind.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors:{
+				azul:"#0065a2",
+				gris:"#878786",
+				
+			}
+		},
+	},
+	plugins: [],
+}
